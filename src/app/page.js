51fd4909c96d6e279c4ab43157ad2a3f9466e1e5
Emma-TE <107342245+Emma-TE@users.noreply.github.com>
2026-02-19 11:57:48 +0100

    first commit

--- src/app/page.js
+++ src/app/page.js
@@ -14,26 +14,10 @@ export default function Home() {
           priority
         />
         <div className={styles.intro}>
-          <h1>To get started, edit the page.js file.</h1>
-          <p>
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
+  <h1>🍽️ Family Meal Planner</h1>
+  <p>
+    Welcome! This will be our private meal planner for us, and guests that wants to view.
+  </p>
         </div>
         <div className={styles.ctas}>
           <a
